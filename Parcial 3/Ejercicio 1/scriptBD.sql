ed04edc55f8a01a3300049c376fe0bd881c0d998
GermanTorres16100283 <78859053+GermanTorres16100283@users.noreply.github.com>
2021-06-02 17:42:17 -0500

    modificacion script

--- Parcial 3/Ejercicio 1/scriptBD.sql
+++ Parcial 3/Ejercicio 1/scriptBD.sql
@@ -6,7 +6,7 @@ create table usuarios
 (
     idUsuario int not null auto_increment,
     usuario varchar(20),
-    correo varchar(50)
+    correo varchar(50),
     password varchar(200),
     primary key (idUsuario)
 )
